7f5ae72248ef036ad064e0f73cdf5462648b887f
Gerallt Franke <geralltf@gmail.com>
2025-09-10 17:46:35 +1000

    C# Compiler.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using SquashC.Compiler;
+
+namespace AST.CS
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Logger.InitLogging();
+
+            Console.WriteLine("*-*-*-*-*-*-* SquashC Compiler *-*-*-*-*-*-*");
+
+            string code = System.IO.File.ReadAllText("./../../../scripts/EntryPoint.c");
+
+            SquashCompiler compiler = new SquashCompiler(code);
+            compiler.CompileExpression();
+        }
+    }
+}
